e65b5ad553d8da9a38c4a43128458d53bff5e95b
ecarrillo99 <74939517+ecarrillo99@users.noreply.github.com>
2024-09-17 21:46:55 -0500

    Añadir entidad Product

--- obj/Debug/net8.0/ProductsApiRest.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductsApiRest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductsApiRest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4985b90e29d2831bd63deba6179355d1f5af25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductsApiRest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductsApiRest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
